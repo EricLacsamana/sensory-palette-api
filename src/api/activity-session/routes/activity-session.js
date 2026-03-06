5ae0850c3cfd8233ce54d85a7ad3d2f48bc81d8b
Eric Lacsamana <eric@Erics-MacBook-Pro.local>
2026-03-06 10:06:54 +0800

    filtering activities

--- src/api/activity-session/routes/activity-session.js
+++ src/api/activity-session/routes/activity-session.js
@@ -1,8 +1,13 @@
-
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::activity-session.activity-session', {
   config: {
+    find: {
+      policies: ['global::filter-by-owner'],
+    },
+    findOne: {
+      policies: ['global::filter-by-owner'],
+    },
     create: {
       policies: ['global::bind-user-therapist'],
     },
